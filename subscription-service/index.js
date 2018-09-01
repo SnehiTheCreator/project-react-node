c5c08d57a5e400fe2bde7119f069eae060f93473
Snehi Vallurupalli <snehiv6@gmail.com>
2018-08-31 22:01:18 -0400

    services setup and tested in postman

--- subscription-service/index.js
+++ subscription-service/index.js
@@ -1,10 +1,17 @@
 const { send, json } = require('micro')
 const { router, post } = require('microrouter')
 
+const subscriptionFactory = (vin, startDate, subscriptionLength) => ({vin, startDate, subscriptionLength});
+
 module.exports = router(
   post('/', async (req, res) => {
     const subscription = await json(req)
-    // TODO: "Save" subscription
-    // TODO: Return response
+    // should validate values and make sure eveything needed is given and in the right format
+    // send back error if not valid
+    
+    //save to db here
+    
+    return send(res, 200, subscription);
+    
   })
 )
--- vehicle-service/index.js
+++ vehicle-service/index.js
@@ -1,14 +1,17 @@
-const { send } = require('micro')
+const { send, json } = require('micro')
 const { router, get } = require('microrouter')
+
+const vehicleFactory = (vin, make, model, year, mileage, imageUrl) => ({vin, make, model, year, mileage, imageUrl});
 
 module.exports = router(
   get('/vehicles', (req, res) => {
-    const vehicles = []
-
-    // TODO: Return list of vehicles
-
-    return {
-      vehicles
-    }
+    const vehicles = [
+      vehicleFactory("0001", "Lamborghini", "Countach", "1985", 25000, 'http://twinrev.com/img/cars/-9223372036852422073/3b0d7288f8af6e0da735321bd7df2907-1329-713'),
+      vehicleFactory("0002", "Mercedes-Benz", "SLS AMG", "2015", 50000, 'https://st.motortrend.com/uploads/sites/10/2017/11/2014-mercedes-benz-sls-amg-coupe-angular-front.png'),
+      vehicleFactory("0002", "Land Rover", "Range Rover Sport", "2017", 40000, 'https://st.motortrend.com/uploads/sites/10/2015/11/2011-land-rover-range-rover-hse-4wd-suv-angular-front.png')
+    ];
+    
+    
+    return vehicles;
   })
 )
